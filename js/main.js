225390a046f49b432897c54e2854bce87d9ae4a6
Frob the Builder <ebanyash@gmail.com>
2014-10-22 01:11:54 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
+	adjust();
+	$(window).resize(function() {
+		adjust();
+	})
+})
 
+function adjust() {
+	$(".jumbo-slime").css({
+		top: (window.innerHeight/2) - ($(".jumbo-slime").outerHeight(false)/2),
+		left: (window.innerWidth/2) - ($(".jumbo-slime").outerWidth(false)/2)
+	})
+	//$("body").css({
+	//	"background-size": "auto " + window.innerHeight + "px"
+	//})
+}
